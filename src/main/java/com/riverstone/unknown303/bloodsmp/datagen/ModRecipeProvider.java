8d7b419b9e1ff97269b883efb93a41d2dd8c6d83
Unknown_303YT <unknown.303yt@gmail.com>
2024-11-17 09:19:46 +1030

    Add DataGen
    Signed-off-by: Unknown_303YT <unknown.303yt@gmail.com>

---
+++ src/main/java/com/riverstone/unknown303/bloodsmp/datagen/ModRecipeProvider.java
@@ -0,0 +1,30 @@
+package com.riverstone.unknown303.bloodsmp.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.minecraft.data.server.recipe.RecipeExporter;
+import net.minecraft.data.server.recipe.RecipeGenerator;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModRecipeProvider extends FabricRecipeProvider {
+    public ModRecipeProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+        super(output, registriesFuture);
+    }
+
+    @Override
+    protected RecipeGenerator getRecipeGenerator(RegistryWrapper.WrapperLookup registryLookup, RecipeExporter exporter) {
+        return new RecipeGenerator(registryLookup, exporter) {
+            @Override
+            public void generate() {
+
+            }
+        };
+    }
+
+    @Override
+    public String getName() {
+        return "BloodSMP Recipes";
+    }
+}
